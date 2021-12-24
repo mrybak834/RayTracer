4c23f359f59d5d5da9f7a30a9737ce1410f94332
mrybakie <marek.rybakiewicz@peapod.com>
2021-12-24 14:10:38 -0600

    Matrix

--- src/main/kotlin/model/matrix/Matrix.kt
+++ src/main/kotlin/model/matrix/Matrix.kt
@@ -1,9 +1,39 @@
 package model.matrix
 
+import util.equalsE
+
 data class Matrix(
     val matrix: List<List<Double>>
 ) {
     operator fun get(row: Int, col: Int): Double {
         return matrix[row][col]
+    }
+
+    override operator fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other == null || this::class != other::class) return false
+        other as Matrix
+
+        for (col in matrix.indices) {
+            for (row in matrix[col].indices) {
+                if (!(matrix[col][row].equalsE(other.matrix[col][row]))) return false
+            }
+        }
+
+        if (matrix != other.matrix) return false
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return matrix.hashCode()
+    }
+
+    fun print() {
+        for (row in matrix) {
+            for (col in row) {
+                print("$col ")
+            }
+            println()
+        }
     }
 }
--- src/test/kotlin/matrix/MatrixTest.kt
+++ src/test/kotlin/matrix/MatrixTest.kt
@@ -20,6 +20,88 @@ internal class MatrixTest {
         )
 
         assertTrue(m[0, 0].equalsE(1.0))
+        assertTrue(m[0, 3].equalsE(4.0))
+        assertTrue(m[1, 0].equalsE(5.5))
+        assertTrue(m[1, 2].equalsE(7.5))
+        assertTrue(m[2, 2].equalsE(11.0))
+        assertTrue(m[3, 0].equalsE(13.5))
+        assertTrue(m[3, 2].equalsE(15.5))
+    }
+
+    @Test
+    fun `Create a 2x2 matrix`() {
+        val m = Matrix(
+            listOf(
+                listOf(-3.0, 5.0),
+                listOf(1.0, -2.0)
+            )
+        )
+
+        assertTrue(m[0, 0].equalsE(-3.0))
+        assertTrue(m[0, 1].equalsE(5.0))
+        assertTrue(m[1, 0].equalsE(1.0))
+        assertTrue(m[1, 1].equalsE(-2.0))
+    }
+
+    @Test
+    fun `Create a 3x3 matrix`() {
+        val m = Matrix(
+            listOf(
+                listOf(-3.0, 5.0, 0.0),
+                listOf(1.0, -2.0, -7.0),
+                listOf(0.0, 1.0, 1.0)
+            )
+        )
+
+        assertTrue(m[0, 0].equalsE(-3.0))
+        assertTrue(m[1, 1].equalsE(-2.0))
+        assertTrue(m[2, 2].equalsE(1.0))
+    }
+
+    @Test
+    fun `Identical matrices are equal`() {
+        val a = Matrix(
+            listOf(
+                listOf(1.0, 2.0, 3.0, 4.0),
+                listOf(5.0, 6.0, 7.0, 8.0),
+                listOf(9.0, 8.0, 7.0, 6.0),
+                listOf(5.0, 4.0, 3.0, 2.0)
+            )
+        )
+
+        val b = Matrix(
+            listOf(
+                listOf(1.0, 2.0, 3.0, 4.0),
+                listOf(5.0, 6.0, 7.0, 8.0),
+                listOf(9.0, 8.0, 7.0, 6.0),
+                listOf(5.0, 4.0, 3.0, 2.0)
+            )
+        )
+
+        assertTrue(a == b)
+    }
+
+    @Test
+    fun `Dissimilar matrices are not equal`() {
+        val a = Matrix(
+            listOf(
+                listOf(1.0, 2.0, 3.0, 4.0),
+                listOf(5.0, 6.0, 7.0, 8.0),
+                listOf(9.0, 8.0, 7.0, 6.0),
+                listOf(5.0, 4.0, 3.0, 2.0)
+            )
+        )
+
+        val b = Matrix(
+            listOf(
+                listOf(2.0, 3.0, 4.0, 5.0),
+                listOf(6.0, 7.0, 8.0, 9.0),
+                listOf(8.0, 7.0, 6.0, 5.0),
+                listOf(4.0, 3.0, 2.0, 1.0)
+            )
+        )
+
+        assertTrue(a != b)
     }
 
 }
